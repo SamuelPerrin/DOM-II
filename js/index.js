94fbd860c4a8a8802a7a39f4b48de8a4789c145a
MalHibou <swperrin51@gmail.com>
2021-01-12 16:34:18 -0600

    get up to ten different event types

--- js/index.js
+++ js/index.js
@@ -12,15 +12,25 @@ const btn1 = document.querySelector('.btn');
 const btn2 = document.querySelector('.destination').nextElementSibling.querySelector('.btn');
 const btn3 = document.querySelector('.destination').nextElementSibling.nextElementSibling.querySelector('.btn');
 const btns = [btn1, btn2, btn3];
+const imgList = Array.from(document.querySelectorAll('img'))
+imgList.forEach(x => x.style.opacity = 1);
+const pList = Array.from(document.querySelectorAll('p'));
+const h2List = Array.from(document.querySelectorAll('h2'));
+const h4List = Array.from(document.querySelectorAll('h4'));
+const textList = pList.concat(h2List,h4List);
 
 // Event Listeners
-Array.from(nav.children).forEach(x => x.addEventListener('mouseover',grayFont));
+nav.addEventListener('focusin', dottedBorder)
+Array.from(nav.children).forEach(x => x.addEventListener('mouseover', grayFont));
 Array.from(nav.children).forEach(x => x.addEventListener('mouseout', blackFont));
-Array.from(nav.children).forEach(x => x.addEventListener('focusin',dottedBorder));
-Array.from(nav.children).forEach(x => x.addEventListener('focusout',noBorder))
-btns.forEach(x => x.addEventListener('click',signUp));
-window.addEventListener('wheel',showModal);
-window.addEventListener('keydown',hideModal);
+Array.from(nav.children).forEach(x => x.addEventListener('focusin', dottedBorder));
+Array.from(nav.children).forEach(x => x.addEventListener('focusout', noBorder))
+btns.forEach(x => x.addEventListener('click', signUp));
+window.addEventListener('wheel', showModal);
+window.addEventListener('keydown', hideModal);
+window.addEventListener('resize', decreaseOpacity);
+imgList.forEach(x => x.addEventListener('dblclick', swapSource))
+textList.forEach(x => x.addEventListener('copy', changeText))
 
 // Create Modal
 const modal = document.createElement('div');
@@ -37,15 +47,18 @@ modal.appendChild(modalTitle);
 modal.appendChild(modalP1);
 modal.appendChild(modalP2)
 document.body.appendChild(modal);
+modal.style.boxSizing = "border-box";
+modal.style.padding = "10px";
 modal.style.display = "none";
 modal.style.position = "fixed";
 modal.style.top = "50%";
 modal.style.left = "50%";
 modal.style.transform = "translate(-50%, -50%)";
-modal.style.zIndex = '1';
+// modal.style.zIndex = '1';
 modal.style.width = '30vw';
 modal.style.height = '60vh';
 modal.style.backgroundColor = "#ffebcd";
+
 
 // Event Handlers
 function grayFont(e) {
@@ -81,4 +94,27 @@ function dottedBorder(e) {
 
 function noBorder(e) {
     e.target.style.border = "";
+}
+
+function decreaseOpacity(e) {
+    imgList.forEach(x => {
+        x.style.opacity -= 0.1;
+    })
+}
+
+function swapSource(e) {
+    console.log(e.target)
+    let other;
+    do {
+        other = imgList[Math.floor(Math.random() * 4)];
+        console.log(other)
+    } while (other === e.target)
+    const third = e.target.src;
+    e.target.src = other.src;
+    other.src = third;
+}
+
+function changeText(e) {
+    console.log(e)
+    e.target.textContent = "Why are you trying to copy this? Please don't.";
 }
