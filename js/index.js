6ee478ac15cab5f8647d484e18f8be4fcbf0104f
MalHibou <swperrin51@gmail.com>
2021-01-12 16:55:31 -0600

    add stopPropagation()

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 // Selectors
-const header = document.querySelector('header')
-const logo = document.querySelector('.logo-heading');
-const nav = document.querySelector('nav')
-const home = nav.firstElementChild
-const about = home.nextElementSibling;
-const blog = about.nextElementSibling;
-const contact = blog.nextElementSibling;
-const bus = document.querySelector('img');
-const welcome = document.querySelector('.intro h2');
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const cards = Array.from(document.querySelectorAll('.destination'));
 const btn1 = document.querySelector('.btn');
 const btn2 = document.querySelector('.destination').nextElementSibling.querySelector('.btn');
 const btn3 = document.querySelector('.destination').nextElementSibling.nextElementSibling.querySelector('.btn');
 const btns = [btn1, btn2, btn3];
-const imgList = Array.from(document.querySelectorAll('img'))
+const imgList = Array.from(document.querySelectorAll('img'));
 imgList.forEach(x => x.style.opacity = 1);
 const pList = Array.from(document.querySelectorAll('p'));
 const h2List = Array.from(document.querySelectorAll('h2'));
@@ -20,17 +14,17 @@ const h4List = Array.from(document.querySelectorAll('h4'));
 const textList = pList.concat(h2List,h4List);
 
 // Event Listeners
-nav.addEventListener('focusin', dottedBorder)
 Array.from(nav.children).forEach(x => x.addEventListener('mouseover', grayFont));
 Array.from(nav.children).forEach(x => x.addEventListener('mouseout', blackFont));
 Array.from(nav.children).forEach(x => x.addEventListener('focusin', dottedBorder));
-Array.from(nav.children).forEach(x => x.addEventListener('focusout', noBorder))
+Array.from(nav.children).forEach(x => x.addEventListener('focusout', noBorder));
 btns.forEach(x => x.addEventListener('click', signUp));
+cards.forEach(x => x.addEventListener('click', addBoxShadow))
 window.addEventListener('wheel', showModal);
 window.addEventListener('keydown', hideModal);
 window.addEventListener('resize', decreaseOpacity);
-imgList.forEach(x => x.addEventListener('dblclick', swapSource))
-textList.forEach(x => x.addEventListener('copy', changeText))
+imgList.forEach(x => x.addEventListener('dblclick', swapSource));
+textList.forEach(x => x.addEventListener('copy', changeText));
 
 // Create Modal
 const modal = document.createElement('div');
@@ -42,10 +36,10 @@ modalP1.textContent = "Subscribe to our newsletter for all the latest updates!";
 modalP1.style.fontSize = "3rem";
 modalP1.style.textAlign = "center";
 const modalP2 = document.createElement('p');
-modalP2.textContent = "To close this window, press Escape"
+modalP2.textContent = "To close this window, press Escape";
 modal.appendChild(modalTitle);
 modal.appendChild(modalP1);
-modal.appendChild(modalP2)
+modal.appendChild(modalP2);
 document.body.appendChild(modal);
 modal.style.boxSizing = "border-box";
 modal.style.padding = "10px";
@@ -54,7 +48,6 @@ modal.style.position = "fixed";
 modal.style.top = "50%";
 modal.style.left = "50%";
 modal.style.transform = "translate(-50%, -50%)";
-// modal.style.zIndex = '1';
 modal.style.width = '30vw';
 modal.style.height = '60vh';
 modal.style.backgroundColor = "#ffebcd";
@@ -71,6 +64,7 @@ function blackFont(e) {
 
 function signUp (e) {
     alert("You have signed up!");
+    e.stopPropagation();
 }
 
 function showModal(e) {
@@ -79,6 +73,7 @@ function showModal(e) {
         modal.style.flexDirection = "column";
         modal.style.justifyContent = "space-evenly";
         modal.style.alignItems = "center";
+        window.removeEventListener('wheel',showModal);
     }
 }
 
@@ -117,4 +112,8 @@ function swapSource(e) {
 function changeText(e) {
     console.log(e)
     e.target.textContent = "Why are you trying to copy this? Please don't.";
+}
+
+function addBoxShadow(e) {
+    e.target.parentElement.style.boxShadow = "0 0 5px gray";
 }
